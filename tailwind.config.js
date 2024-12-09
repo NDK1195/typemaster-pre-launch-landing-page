61a83bc42c8e30a85dba59d698b2cf181387e2fe
ndk1195 <ndkhoa1195@gmail.com>
2024-12-09 15:35:19 +0700

    finish project

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,26 @@ module.exports = {
         "#7B8BAD": "#7B8BAD",
         "#E8EFF2": "#E8EFF2",
       },
+      backgroundImage: {
+        "phone-and-keyboard-mobile":
+          "url('../assets/mobile/image-phone-and-keyboard.jpg')",
+        "phone-and-keyboard-tablet":
+          "url('../assets/tablet/image-phone-and-keyboard.jpg')",
+        "phone-and-keyboard-desktop":
+          'url("../assets/desktop/image-phone-and-keyboard.jpg")',
+        "glass-and-keyboard-mobile":
+          'url("../assets/mobile/image-glass-and-keyboard.jpg")',
+        "glass-and-keyboard-tablet":
+          'url("../assets/tablet/image-glass-and-keyboard.jpg")',
+        "glass-and-keyboard-desktop":
+          'url("../assets/desktop/image-glass-and-keyboard.jpg")',
+        "keyboard-desktop": 'url("../assets/desktop/image-keyboard.jpg")',
+        "keyboard-tablet": 'url("../assets/tablet/image-keyboard.jpg")',
+        "keyboard-mobile": 'url("../assets/mobile/image-keyboard.jpg")',
+      },
+      screens: {
+        "2xl": "1440px",
+      },
     },
   },
   plugins: [],
